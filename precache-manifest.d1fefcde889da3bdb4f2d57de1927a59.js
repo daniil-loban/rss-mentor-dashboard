1e3b14002a536d6e9dc28b0ea641cdbd4d998bbb
daniil-loban <dungeon_lda@mail.ru>
2019-02-24 20:32:39 +0300

    test: commit may not works

---
+++ precache-manifest.d1fefcde889da3bdb4f2d57de1927a59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "729bfe719c7c22c0333f",
+    "url": "/static/css/main.cf0179c4.chunk.css"
+  },
+  {
+    "revision": "729bfe719c7c22c0333f",
+    "url": "/static/js/main.729bfe71.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b1320ae8331f25190098",
+    "url": "/static/js/2.b1320ae8.chunk.js"
+  },
+  {
+    "revision": "2fa95c7c4c32636e0ba2dfdd15548901",
+    "url": "/index.html"
+  }
+];
